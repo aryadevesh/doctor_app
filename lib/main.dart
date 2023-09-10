3e9687cd4f536958d8aa174e34af2e48a126c73d
aryadevesh <128689265+aryadevesh@users.noreply.github.com>
2023-09-10 23:16:50 +0530

    last commit gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@
 import 'package:doctor_app/splashScreen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 import 'infoHandler/app_info.dart';
 
 void main() async
 {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
